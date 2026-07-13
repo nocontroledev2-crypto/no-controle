d76ec7df440b152499f108c8ce27950fdae4e004
nocontroledev2-crypto <nocontrole.dev.2@gmail.com>
2026-07-13 12:36:37 +0000

    feat: adiciona compras online e bancos ao dicionario brasil

--- app/(tabs)/registrar.tsx
+++ app/(tabs)/registrar.tsx
@@ -138,8 +138,7 @@ export default function Registrar() {
     recognition.onresult = (event: any) => {
       const textoFalado = event.results[0][0].transcript;
          console.log(textoFalado);
-         alert(textoFalado);
-      setState("processing");
+       
 
       const parsed = parseSpeech(textoFalado);
 
