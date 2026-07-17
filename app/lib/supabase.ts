431f5504c8716cd0a79706721df86ea0d14cc32b
nocontroledev2-crypto <nocontrole.dev.2@gmail.com>
2026-07-16 21:14:34 -0300

    feat: adiciona cliente supabase e servico de autenticacao

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,9 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "COLE_AQUI_SUA_PROJECT_URL";
+const SUPABASE_URL = "https://udiencdsidiqxinxqgjs.supabase.co";
 
 const SUPABASE_ANON_KEY =
-  sb_publishable_mOvzmivp33ka-tgZxS7QJg_YgEvakg3;
+  "sb_publishable_mOvzmivp33ka-tgZxS7QJg_YgEvakg3";
 
 export const supabase = createClient(
   SUPABASE_URL,
