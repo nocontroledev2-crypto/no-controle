d771d648ddcb7a400bac5dd917e192c9c07c1db1
nocontroledev2-crypto <nocontrole.dev.2@gmail.com>
2026-08-04 18:54:40 -0300

    TURBO-020D-Fix-Duplicidade-Falta-Registro

--- app/evolucao-total.tsx
+++ app/evolucao-total.tsx
@@ -2671,42 +2671,7 @@ const deveMostrarHojeVsMedia =
   mediaDiasComMovimentacao > 0 &&
   indiceHojeNoGrafico >= 0;
 
-  const finalDoPeriodoComMuitosZeros = (() => {
-  if (safeChartValues.length < 6) {
-    return false;
-  }
-
-  const ultimoIndiceComGasto = safeChartValues.reduce(
-    (ultimo, value, index) =>
-      Number(value) > 0 ? index : ultimo,
-    -1
-  );
-
-  if (ultimoIndiceComGasto < 0) {
-    return false;
-  }
-
-  const pontosSemGastoNoFinal =
-    safeChartValues.length - 1 - ultimoIndiceComGasto;
-
-  if (unidadeSingular === "mês") {
-    return pontosSemGastoNoFinal >= 2;
-  }
-
-  if (unidadeSingular === "dia") {
-    return pontosSemGastoNoFinal >= 3;
-  }
-
-  return false;
-})();
-
-const deveTratarQuedaComoPossivelFaltaRegistro =
-  finalDoPeriodoComMuitosZeros &&
-  (
-    tipoTendenciaPeriodo === "queda-forte" ||
-    tipoTendenciaPeriodo === "queda-moderada"
-  );
-
+  
 const textoHojeVsMedia = escolherTexto(
   percentualHojeVsMedia >= 20
     ? [
